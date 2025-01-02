68f8c9d9c5537c5b03014d2d464bbb4953b43d4f
Pablo Penovi <pablopenovi@gmail.com>
2025-01-02 21:30:27 +0000

    Solved day 19 part 1

--- d19/d19.go
+++ d19/d19.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-const isDebug = true
+const isDebug = false
 
 func Init(ver constants.VersionIndex) {
   lines, err := io.GetLinesFor(constants.Nineteen, ver)
@@ -21,19 +21,23 @@ func Init(ver constants.VersionIndex) {
 
 func findPossible(available *map[string]struct{}, desired *[]string) int {
   count := 0
+  solved := make(map[string]struct{})
   for _, d := range *desired {
     if isDebug { fmt.Printf("\n**** Desired pattern %s ****\n", d) }
-    res := solve(d, available, 0, len(d))
-    if isDebug { fmt.Printf("Matches for pattern %s: %d\n\n", d, res) }
-    count += res
+    count += solve(d, available, 0, len(d), &solved)
   }
   return count
 }
 
-func solve(d string, a *map[string]struct{}, start int, end int) int {
+func solve(d string, a *map[string]struct{}, start int, end int, solved *map[string]struct{}) int {
+  // Check if this input has already been solved to avoid pursuing unnecesary recursion branches
+  _, isSolved := (*solved)[d]; if isSolved { return 1 }
+
   // End condition 1: End cursor reached start so everything matched
   if end == 0 {
     if isDebug { fmt.Printf("Pattern %s is a match\n", d) }
+    // Add to solved inputs
+    (*solved)[d] = struct{}{}
     return 1
   }
   // End condition 2: Start cursor caught up with end cursor, so no match was found
@@ -42,17 +46,21 @@ func solve(d string, a *map[string]struct{}, start int, end int) int {
     return 0
   }
   
-  // If there is a match, recurse from beginning of input to start of current match
-  // (i.e. we start again but leaving out what has been matched so far)
+  // If there is a match, we branch
+  // Branch 1: Recurse from beginning of input to start of current match (i.e. we start again but leaving out what has been matched so far)
+  // Branch 2: Continue as if this wasn't a match
   _, isMatch := (*a)[d[start:end]]
   if isMatch {
     if isDebug { fmt.Printf("Segment %s is a match\n", d[start:end]) }
-    return solve(d, a, 0, start)
+    res := solve(d, a, start + 1, end, solved)
+    res += solve(d, a, 0, start, solved)
+    if res > 0 { return 1 }
+    return 0
   }
 
   // If no match, advance start by 1 and recurse
   if isDebug { fmt.Printf("Segment %s is NOT a match\n", d[start:end]) }
-  return solve(d, a, start + 1, end)
+  return solve(d, a, start + 1, end, solved)
 }
 
 func parsePatterns(lines *[]string) (*map[string]struct{}, *[]string) {
